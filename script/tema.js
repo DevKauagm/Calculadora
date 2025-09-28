92f2dfab57c05e0035610ff78849066f31091c71
DevKauagm <kauag0988@gmail.com>
2025-09-27 23:14:11 -0300

    up

--- script/tema.js
+++ script/tema.js
@@ -1,15 +1,19 @@
 const header = document.querySelector('header')
 const calculadora = document.getElementById('calculadora')
+const botao = document.querySelectorAll('main > .botao > p')
 
 function tema(tema) {
     switch(tema) {
         case 'claro':
             header.style.backgroundColor = 'var(--cor4)'
-            calculadora.style.backgroundImage = 'linear-gradient(120deg, var(--cor4), var(--cor5), var(--cor6))'
-            document.body.style.backgroundImage = 'linear-gradient()'
+            document.body.style.backgroundImage = 'linear-gradient(120deg, var(--cor4), var(--cor5), var(--cor6))'
+            calculadora.style.backgroundColor = 'var(--cor1)'
+            botao.forEach(p => {p.style.color = 'var(--cor2)'})
             break
         case 'escuro':
             header.style = ''
+            document.body.style = ''
             calculadora.style = ''
+            botao.forEach(p => {p.style = ''})
     }
 }
